ea0dd777c5f03a257f6255c81a26a9b2b0daa479
Vitor Marques <v.marques.rodrigues@avanade.com>
2025-12-16 16:20:06 -0300

    feat(Solution): Solve foundations/06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (str, repeatNum) {
+  if (repeatNum < 0) {
+    return "ERROR";
+  }
+  let returnStr = "";
+  for (let i = 0; i < repeatNum; i++) {
+    returnStr = returnStr.concat(str);
+  }
+  return returnStr;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
