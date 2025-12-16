b59a7779a57e1f75736c558f353db4338e3b979e
Vitor Marques <v.marques.rodrigues@avanade.com>
2025-12-16 17:19:48 -0300

    feat(Solution): Solve foundations/11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let celsiusTemp = (temp - 32) * (5 / 9);
+  if (celsiusTemp === Math.round(celsiusTemp)) return celsiusTemp;
+  return +celsiusTemp.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let fahrenheitTemp = temp * (9 / 5) + 32;
+  if (fahrenheitTemp === Math.round(fahrenheitTemp)) return fahrenheitTemp;
+  return +fahrenheitTemp.toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
+
