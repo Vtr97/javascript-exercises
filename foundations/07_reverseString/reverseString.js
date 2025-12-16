813ce10c9b8b22577c00beb82a7990525d52b171
Vitor Marques <v.marques.rodrigues@avanade.com>
2025-12-16 16:53:02 -0300

    feat(Solution): Solve foundations/07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  if (typeof str != "string") {
+    return "Input is not a string";
+  }
+  let str_array = str.split("");
+  return str_array.reverse().join("");
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
