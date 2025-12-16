b35107c357c140d03a87412e64b7f58c8baf63b0
Vitor Marques <v.marques.rodrigues@avanade.com>
2025-12-16 16:53:14 -0300

    feat(Solution): Solve foundation/08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  if (!Array.isArray(arr)) {
+    return;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (args.includes(arr[i])) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
