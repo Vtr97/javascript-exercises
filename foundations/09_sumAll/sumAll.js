e36c7bbc46de4755509951afa21d2137ec07570d
Vitor Marques <v.marques.rodrigues@avanade.com>
2025-12-16 16:53:29 -0300

    feat(Solution): Solve foundations/09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    return "ERROR";
+  }
+  if (a < 0 || b < 0) {
+    return "ERROR";
+  }
+  if (a !== Math.floor(a) || b !== Math.floor(b)) {
+    return "ERROR";
+  }
+  biggerNumber = Math.max(a, b);
+  lowerNumber = Math.min(a, b);
+  let sum = 0;
+  for (let i = lowerNumber; i <= biggerNumber; i++) {
+    sum += i;
+  }
 
+  return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
