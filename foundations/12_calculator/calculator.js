72e4772f2e173b5eb27f92ceb89bb6b974a80978
Vitor Marques <v.marques.rodrigues@avanade.com>
2025-12-18 15:33:10 -0300

    feat(Solution): Solve foundations/12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  if (!Array.isArray(arr)) return;
+  return arr.reduce((acc, num) => (acc += num), 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  if (!Array.isArray(arr)) return;
+  return arr.reduce((acc, num) => (acc *= num), 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  let pow = 1;
+  if (b === 0) return 1;
+  for (let i = 1; i <= b; i++) {
+    pow *= a;
+  }
+  if (b < 0) {
+    return 1 / pow;
+  }
+  return pow;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let factorial = 1;
+  if (a === 0) return 1;
+  for (let i = 1; i <= a; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
@@ -29,5 +44,6 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
+
