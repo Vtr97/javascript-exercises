4b1736fff24ecda62dd93b1e04153633c1ea255d
Vitor Marques <v.marques.rodrigues@avanade.com>
2025-12-18 16:52:49 -0300

    feat(Solution): Solve fundamentals/16
    This commit completes the foundations javascript exercises

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  function getAge(person) {
+    const deathDate = person.yearOfDeath ?? new Date().getFullYear();
+    return deathDate - person.yearOfBirth;
+  }
+  function getOldest(personA, personB) {
+    if (getAge(personA) >= getAge(personB)) return personA;
+    return personB;
+  }
+  return array.reduce(
+    (oldest, person) => {
+      return getOldest(oldest, person);
+    },
+    { yearOfDeath: 0, yearOfBirth: 0 }
+  );
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
