7ab96e8e55ec4032bc6e41142dd583e412a348c7
Vitor Marques <v.marques.rodrigues@avanade.com>
2025-12-18 16:26:53 -0300

    feat(Solution): Solve foundations/14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (input) {
+  let numInput = +input;
+  if (numInput < 0) return "OOPS";
+  if (numInput === 0) return 0;
+  if (numInput === 1) return 1;
+  if (numInput === 2) return 1;
+  return fibonacci(numInput - 2) + fibonacci(numInput - 1);
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
